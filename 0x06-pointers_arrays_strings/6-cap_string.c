3d6b1a7e6d18bb793fc3fcf187f1b8f714cfd69b
Aishatu <aishatuzakariibrahim@gmail.com>
2022-10-28 03:46:49 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,12 +16,12 @@ char *cap_string(char *s)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
-			s[0] = s[0] -32;
+			s[0] = s[0] - 32;
 		}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
 				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-				s[count] == '"' || s[count] == '(' || s[count] ==')'
+				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
 				|| s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
